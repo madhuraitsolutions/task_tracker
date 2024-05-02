2f82e4a8b333bf697733cdebb8b46f72b7aa8aa9
Kombiah <kombiahrk@yahoo.com>
2024-05-02 15:48:10 +0530

    Imported AddTask Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import AddTask from './components/AddTask'
 
 function App() {
 
@@ -8,9 +9,11 @@ function App() {
       <h1 className="text-3xl font-bold py-4 pl-6">
         03 - TaskTracker
       </h1>
-      <p className='text-xl pl-6'>
-        Click here to add new tasks
-      </p>
+      <div className='flex flex-row items-center gap-4'>
+        <p className='text-xl pl-6'>Click </p>
+        <AddTask />
+        <p className='text-xl'>to add new task</p>
+      </div>
     </>
   )
 }
