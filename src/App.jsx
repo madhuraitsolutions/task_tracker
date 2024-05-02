ad4e6c4a2efbfcaf80bdda6a18af613afb3739bb
Kombiah <kombiahrk@yahoo.com>
2024-05-02 18:08:47 +0530

    Added Todo List

--- src/App.jsx
+++ src/App.jsx
@@ -13,14 +13,21 @@ function App() {
       </h1>
       <div className='flex flex-row items-center gap-4'>
         <p className='text-xl pl-6'>Click </p>
-        <AddTask taskList={taskList} setTaskList={setTaskList}/>
+        <AddTask taskList={taskList} setTaskList={setTaskList} />
         <p className='text-xl'>to add new task</p>
       </div>
-      {
-        taskList.map((task, index) => (
-          <Todo key={index} task={task} />
-        ))
-      }
+      <div>
+        <h1 className="ml-6 my-2 bg-slate-600 w-4/12 text-2xl font-bold py-1 px-4 rounded-lg">
+          Todo List
+        </h1>
+        {
+          taskList.map((task, index) => (
+            <>
+              <Todo key={index} task={task} />
+            </>
+          ))
+        }
+      </div>
     </>
   )
 }
