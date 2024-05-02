c3621499c307549a840a112a0c03a3ecb3cbe4a4
Kombiah <kombiahrk@yahoo.com>
2024-05-02 19:17:49 +0530

    EditTask Component Updated to Edit the task and fetch project name using useEffect()

--- src/components/EditTask.jsx
+++ src/components/EditTask.jsx
@@ -1,29 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-export const EditTask = (task, taskList, setTaskList) => {
+export const EditTask = ({task, taskList, setTaskList}) => {
     const [editModel, setEditModel] = React.useState(false)
-    const [projectName, setProjectName] = React.useState('')
-    const [taskDescription, setTaskDescription] = React.useState('')
+    const [projectName, setProjectName] = useState('')
+    const [taskDescription, setTaskDescription] = useState('')
+
+    useEffect(() => {
+        setProjectName(task.projectName)
+        setTaskDescription(task.taskDescription)
+    }, [])
 
     const handleEditTask = (e) => {
         e.preventDefault()
-        setTaskList([...taskList, {projectName, taskDescription}])
+        let taskIndex = taskList.indexOf(task)
+        taskList.splice(taskIndex, 1)
+        setTaskList([...taskList, { projectName, taskDescription }])
         setEditModel(false)
-        setProjectName('')
-        setTaskDescription('')
     }
 
     const handleInputChange = (e) => {
-        // const name = e.target.name;
-        // const value = e.target.value;
-        const {name, value} = e.target;
-        if(name === 'projectName') setProjectName(value)
-        if(name === 'taskDescription') setTaskDescription(value)
+        const { name, value } = e.target;
+        if (name === 'projectName') setProjectName(value)
+        if (name === 'taskDescription') setTaskDescription(value)
     }
     return (
         <>
             <button
                 className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+                onClick={() => setEditModel(true)}
             >
                 Edit
             </button>
@@ -41,40 +45,40 @@ export const EditTask = (task, taskList, setTaskList) => {
                             </div>
                             <form className='p-6'>
                                 <div>
-                                <label
-                                    className='text-xl uppercase font-semibold mb-2 block'
-                                    htmlFor="project-name"
-                                >
-                                    Project Name
-                                </label>
-                                <input
-                                    className='w-full rounded-lg border px-5 py-2'
-                                    id='project-name'
-                                    name='projectName'
-                                    type="text"
-                                    value={projectName}
-                                    onChange={handleInputChange}
-                                    placeholder='Project Name'
-                                    required
-                                />
+                                    <label
+                                        className='text-xl uppercase font-semibold mb-2 block'
+                                        htmlFor="project-name"
+                                    >
+                                        Project Name
+                                    </label>
+                                    <input
+                                        className='w-full rounded-lg border px-5 py-2'
+                                        id='project-name'
+                                        name='projectName'
+                                        type="text"
+                                        value={projectName || ''}
+                                        onChange={handleInputChange}
+                                        placeholder='Project Name'
+                                        required
+                                    />
                                 </div>
                                 <div>
-                                <label className='text-xl uppercase font-semibold m-2 block'>
-                                    Task Description
-                                </label>
-                                <textarea 
-                                    id="task-description"
-                                    rows="3"
-                                    name="taskDescription"
-                                    value={taskDescription}
-                                    onChange={handleInputChange}
-                                    placeholder='Task Description'
-                                    className='w-full rounded-lg border px-5 py-2'
-                                />
+                                    <label className='text-xl uppercase font-semibold m-2 block'>
+                                        Task Description
+                                    </label>
+                                    <textarea
+                                        id="task-description"
+                                        rows="3"
+                                        name="taskDescription"
+                                        value={taskDescription || ''}
+                                        onChange={handleInputChange}
+                                        placeholder='Task Description'
+                                        className='w-full rounded-lg border px-5 py-2'
+                                    />
                                 </div>
                             </form>
                             <div className='flex justify-end p-6 border-t-2 border-slate-200'>
-                                <button 
+                                <button
                                     type="submit"
                                     className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
                                     onClick={handleEditTask}
