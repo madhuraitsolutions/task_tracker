6d6d1fdeb3d5eebd97b4a838c601461e0cada286
Kombiah <kombiahrk@yahoo.com>
2024-05-02 18:17:20 +0530

    Imported EditTask Component

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { EditTask } from './EditTask'
 
 const Todo = ({ task }) => {
     return (
@@ -6,7 +7,7 @@ const Todo = ({ task }) => {
             <div className='flex flex-col items-start justify-start bg-slate-600 w-4/12 py-4 px-6 ml-6 my-4 rounded-lg'>
                 <div className='w-full flex flex-row justify-between'>
                     <p className='text-2xl'>{task.projectName}</p>
-                    <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Edit</button>
+                    <EditTask/>
                 </div>
                 <p className='text-xl'>{task.taskDescription}</p>
                 <div className='w-full flex justify-center'>                    
