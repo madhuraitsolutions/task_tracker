db27c01a6028ae998e46b8fc12b4647dc60891a6
Kombiah <kombiahrk@yahoo.com>
2024-05-02 18:13:59 +0530

    TodoComponent Edit and Delete Button Added

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -4,8 +4,14 @@ const Todo = ({ task }) => {
     return (
         <>
             <div className='flex flex-col items-start justify-start bg-slate-600 w-4/12 py-4 px-6 ml-6 my-4 rounded-lg'>
-                <p className='text-2xl'>{task.projectName}</p>
+                <div className='w-full flex flex-row justify-between'>
+                    <p className='text-2xl'>{task.projectName}</p>
+                    <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Edit</button>
+                </div>
                 <p className='text-xl'>{task.taskDescription}</p>
+                <div className='w-full flex justify-center'>                    
+                    <button className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded'>Delete</button>
+                </div>
             </div>
         </>
     )
