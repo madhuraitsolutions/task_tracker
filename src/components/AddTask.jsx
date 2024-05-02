92204b28dabb0b40d1c955abddf9089132dffa26
Kombiah <kombiahrk@yahoo.com>
2024-05-02 15:47:39 +0530

    AddTask Component and created with rafce

---
+++ src/components/AddTask.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const AddTask = () => {
+    return (
+        <>
+            <button
+                type="button"
+                className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+            >
+                + New
+            </button>
+        </>
+    )
+}
+
+export default AddTask
