d315f7486ecaf5f8859dc8b86ba8e7e8f9cc0c65
Kombiah <kombiahrk@yahoo.com>
2024-05-03 11:07:47 +0530

    Error Handling Added

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,24 +1,35 @@
 import React from 'react'
 
-const AddTask = ({taskList, setTaskList}) => {
+const AddTask = ({ taskList, setTaskList }) => {
     const [addModel, setAddModel] = React.useState(false)
     const [projectName, setProjectName] = React.useState('')
     const [taskDescription, setTaskDescription] = React.useState('')
+    const [errorMessage, setErrorMessage] = React.useState('')
 
     const handleAddTask = (e) => {
         e.preventDefault()
-        setTaskList([...taskList, {projectName, taskDescription}])
-        setAddModel(false)
-        setProjectName('')
-        setTaskDescription('')
+        if (projectName === '') {
+            setErrorMessage('Project name cannot be empty')
+        }
+        else {
+            setTaskList([...taskList, { projectName, taskDescription }])
+            setAddModel(false)
+            setProjectName('')
+            setTaskDescription('')
+        }
+
     }
 
     const handleInputChange = (e) => {
         // const name = e.target.name;
         // const value = e.target.value;
-        const {name, value} = e.target;
-        if(name === 'projectName') setProjectName(value)
-        if(name === 'taskDescription') setTaskDescription(value)
+        const { name, value } = e.target;
+        if (name === 'projectName') {
+            setProjectName(value)
+            setErrorMessage('')
+        }
+        if (name === 'projectName' && value === '') setErrorMessage('Project name cannot be empty')
+        if (name === 'taskDescription') setTaskDescription(value)
     }
 
     return (
@@ -44,40 +55,41 @@ const AddTask = ({taskList, setTaskList}) => {
                             </div>
                             <form className='p-6'>
                                 <div>
-                                <label
-                                    className='text-xl uppercase font-semibold mb-2 block'
-                                    htmlFor="project-name"
-                                >
-                                    Project Name
-                                </label>
-                                <input
-                                    className='w-full rounded-lg border px-5 py-2'
-                                    id='project-name'
-                                    name='projectName'
-                                    type="text"
-                                    value={projectName}
-                                    onChange={handleInputChange}
-                                    placeholder='Project Name'
-                                    required
-                                />
+                                    <label
+                                        className='text-xl uppercase font-semibold mb-2 block'
+                                        htmlFor="project-name"
+                                    >
+                                        Project Name
+                                    </label>
+                                    <input
+                                        className='w-full rounded-lg border px-5 py-2'
+                                        id='project-name'
+                                        name='projectName'
+                                        type="text"
+                                        value={projectName}
+                                        onChange={handleInputChange}
+                                        placeholder='Project Name'
+                                        required
+                                    />
+                                    <p className='text-red-500 text-center'>{errorMessage}</p>
                                 </div>
                                 <div>
-                                <label className='text-xl uppercase font-semibold m-2 block'>
-                                    Task Description
-                                </label>
-                                <textarea 
-                                    id="task-description"
-                                    rows="3"
-                                    name="taskDescription"
-                                    value={taskDescription}
-                                    onChange={handleInputChange}
-                                    placeholder='Task Description'
-                                    className='w-full rounded-lg border px-5 py-2'
-                                />
+                                    <label className='text-xl uppercase font-semibold m-2 block'>
+                                        Task Description
+                                    </label>
+                                    <textarea
+                                        id="task-description"
+                                        rows="3"
+                                        name="taskDescription"
+                                        value={taskDescription}
+                                        onChange={handleInputChange}
+                                        placeholder='Task Description'
+                                        className='w-full rounded-lg border px-5 py-2'
+                                    />
                                 </div>
                             </form>
                             <div className='flex justify-end p-6 border-t-2 border-slate-200'>
-                                <button 
+                                <button
                                     type="submit"
                                     className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
                                     onClick={handleAddTask}
